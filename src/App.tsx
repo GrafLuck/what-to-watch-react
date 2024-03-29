4584ec9ccf51d34363f8f97b0ab4e0f1a893a61f
Dmitry Lutsishin <graf.luck@gmail.com>
2024-03-29 16:19:17 +0300

    первый коммит

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello World</div>
+  }
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
+
+
