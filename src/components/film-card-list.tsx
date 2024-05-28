6072b282892b61fa8a59bfacaa8109f2e0a92062
Dmitry Lutsishin <graf.luck@gmail.com>
2024-05-28 11:51:24 +0300

    выделил компоненты из Main страницы

---
+++ src/components/film-card-list.tsx
@@ -0,0 +1,16 @@
+import SmallFilmCard from './small-film-card';
+
+export default function FilmCardList() {
+  return (
+    <div className="catalog__films-list">
+      <SmallFilmCard />
+      <SmallFilmCard />
+      <SmallFilmCard />
+      <SmallFilmCard />
+      <SmallFilmCard />
+      <SmallFilmCard />
+      <SmallFilmCard />
+      <SmallFilmCard />
+    </div>
+  );
+}
---
+++ src/components/show-more-button.tsx
@@ -0,0 +1,9 @@
+export default function ShowMoreButton() {
+  return (
+    <div className="catalog__more">
+      <button className="catalog__button" type="button">
+        Show more
+      </button>
+    </div>
+  );
+}
