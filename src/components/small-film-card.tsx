0160411d9402fb4a41812befadb03e099ad404dc
Dmitry Lutsishin <graf.luck@gmail.com>
2024-05-29 10:01:42 +0300

    исправил ошибку повторного запуска таймера при убирании курсора с видео

--- src/components/small-film-card.tsx
+++ src/components/small-film-card.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { TSmallCardFilm } from '../types/small-card-film';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 type TSmallFilmCardProps = {
   film: TSmallCardFilm;
@@ -8,25 +8,25 @@ type TSmallFilmCardProps = {
 
 export default function SmallFilmCard({ film }: TSmallFilmCardProps) {
   const [isVideo, setIsVideo] = useState<boolean>(false);
-  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
+  const timeoutId = useRef<NodeJS.Timeout>();
 
   const handleFilmCardMouseOver = () => {
     const id = setTimeout(() => {
       setIsVideo(true);
     }, 1000);
-    setTimeoutId(id);
+    timeoutId.current = id;
   };
 
-  const handleFilmCardMouseLeave = () => {
+  const handleFilmCardMouseOut = () => {
     setIsVideo(false);
-    clearTimeout(timeoutId);
+    clearTimeout(timeoutId.current);
   };
 
   return (
     <article
       className="small-film-card catalog__films-card"
       onMouseOver={handleFilmCardMouseOver}
-      onMouseLeave={handleFilmCardMouseLeave}
+      onMouseOut={handleFilmCardMouseOut}
     >
       <div className="small-film-card__image">
         {!isVideo ? (
@@ -43,7 +43,6 @@ export default function SmallFilmCard({ film }: TSmallFilmCardProps) {
             autoPlay
           />
         )}
-        {/* <img src={film.previewImage} alt={film.name} width={280} height={175} /> */}
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${film.id}`}>
