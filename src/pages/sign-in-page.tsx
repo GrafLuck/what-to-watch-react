6d204c67fb526f044b718c763ea2851b00c74207
Dmitry Lutsishin <graf.luck@gmail.com>
2024-05-30 14:46:00 +0300

    реализовал страницу аутентификации

--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import Footer from '../components/footer';
 import Logo from '../components/logo';
+import { Navigate } from 'react-router-dom';
+import { AppRoute } from '../const';
 
 export default function SignIn() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleFormSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (password && email) {
+      <Navigate to={AppRoute.MAIN} />;
+    }
+  };
+
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
@@ -18,6 +31,8 @@ export default function SignIn() {
                 placeholder="Email address"
                 name="user-email"
                 id="user-email"
+                value={email}
+                onChange={(evt) => setEmail(evt.target.value)}
               />
               <label
                 className="sign-in__label visually-hidden"
@@ -33,6 +48,8 @@ export default function SignIn() {
                 placeholder="Password"
                 name="user-password"
                 id="user-password"
+                value={password}
+                onChange={(evt) => setPassword(evt.target.value)}
               />
               <label
                 className="sign-in__label visually-hidden"
@@ -43,7 +60,11 @@ export default function SignIn() {
             </div>
           </div>
           <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">
+            <button
+              className="sign-in__btn"
+              type="submit"
+              onSubmit={handleFormSubmit}
+            >
               Sign in
             </button>
           </div>
