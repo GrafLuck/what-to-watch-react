91d86c75ea6ad030350ca7cb229936e2b27be352
Dmitry Lutsishin <graf.luck@gmail.com>
2024-05-31 11:12:32 +0300

    реализовал запуск и остановку видео и отображение количества секунд до конца видео

--- src/pages/video-page.tsx
+++ src/pages/video-page.tsx
@@ -1,7 +1,54 @@
+import { useRef, useState } from 'react';
 import { makeFakeFilm } from '../mocks/mocks';
+import { useNavigate } from 'react-router-dom';
+import { getFilmTimeLeft } from '../utils/utils';
 
 export default function VideoPage() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [timeView, setTimeView] = useState(0);
+  const navigate = useNavigate();
+  const videoRef = useRef<HTMLVideoElement | undefined>();
+  let timer = useRef<NodeJS.Timer>();
   const film = makeFakeFilm();
+
+  async function playVideo() {
+    try {
+      if (videoRef?.current) {
+        await videoRef.current.play();
+        setIsPlaying(true);
+        videoRef.current.addEventListener('ended', () => {
+          setIsPlaying(false);
+        });
+        timer = setInterval(() => {
+          if (videoRef.current) {
+            setTimeView(videoRef.current?.currentTime);
+          }
+        }, 1000);
+      }
+    } catch (err) {
+      setIsPlaying(false);
+    }
+  }
+
+  function getTimeLeft() {
+    if (videoRef.current) {
+      return getFilmTimeLeft(timeView, videoRef.current.duration);
+    }
+  }
+
+  const handlePlayButtonClick = () => {
+    if (videoRef.current?.paused) {
+      playVideo();
+    } else {
+      videoRef.current?.pause();
+      clearInterval(timer);
+      setIsPlaying(false);
+    }
+  };
+
+  const handleExitPlayerButtonClick = () => {
+    navigate(-1);
+  };
 
   return (
     <div className="player">
@@ -9,8 +56,13 @@ export default function VideoPage() {
         src={film.videoLink}
         className="player__video"
         poster={film.posterImage}
+        ref={videoRef}
       />
-      <button type="button" className="player__exit">
+      <button
+        type="button"
+        className="player__exit"
+        onClick={handleExitPlayerButtonClick}
+      >
         Exit
       </button>
       <div className="player__controls">
@@ -21,16 +73,31 @@ export default function VideoPage() {
               Toggler
             </div>
           </div>
-          <div className="player__time-value">1:30:29</div>
+          <div className="player__time-value">{getTimeLeft()}</div>
         </div>
         <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width={19} height={19}>
-              <use xlinkHref="#play-s" />
-            </svg>
-            <span>Play</span>
+          <button
+            type="button"
+            className="player__play"
+            onClick={handlePlayButtonClick}
+          >
+            {isPlaying ? (
+              <>
+                <svg viewBox="0 0 14 21" width="14" height="21">
+                  <use xlinkHref="#pause"></use>
+                </svg>
+                <span>Pause</span>
+              </>
+            ) : (
+              <>
+                <svg viewBox="0 0 19 19" width={19} height={19}>
+                  <use xlinkHref="#play-s" />
+                </svg>
+                <span>Play</span>
+              </>
+            )}
           </button>
-          <div className="player__name">Transpotting</div>
+          <div className="player__name">{film.name}</div>
           <button type="button" className="player__full-screen">
             <svg viewBox="0 0 27 27" width={27} height={27}>
               <use xlinkHref="#full-screen" />
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -44,3 +44,17 @@ export function convertMinutesToHoursAndMinutes(minutes: number) {
 export function convertData(date: Date) {
   return date;
 }
+
+export function getFilmTimeLeft(currentTime: number, duration: number) {
+  const timeLeft = duration - currentTime;
+  if (timeLeft < 3600) {
+    const minLeft = Math.floor(timeLeft / 60);
+    const secLeft = Math.floor(timeLeft - minLeft * 60);
+    return `-${minLeft < 10 ? '0' : ''}${minLeft}:${secLeft < 10 ? '0' : ''}${secLeft}`;
+  } else {
+    const hourLeft = Math.floor(timeLeft / 3600);
+    const minLeft = Math.floor((timeLeft - hourLeft * 3600) / 60);
+    const secLeft = Math.floor(timeLeft - hourLeft * 3600 - minLeft * 60);
+    return `-${hourLeft < 10 ? '0' : ''}${hourLeft}:${minLeft < 10 ? '0' : ''}${minLeft}:${secLeft < 10 ? '0' : ''}${secLeft}`;
+  }
+}
